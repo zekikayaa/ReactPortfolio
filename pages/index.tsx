e7e6630ba0edfb2b5008de0974489d75cb449109
Zeki Kaya <zeki.kaya@amerikapostam.com>
2021-11-21 15:58:00 +0300

    Created ServiceCard component
    Design About Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,45 @@
 import { GetServerSideProps } from "next";
-// import { services } from "../data";
+import { IService } from "../type";
+import ServiceCard from "../components/ServiceCard";
 
-const index = (props) => {
+import { services } from "../data";
+
+const index = (props: { services: IService[] }) => {
   // console.log("----------------------------Client----------------------------");
-  // console.log(props)
 
   return (
-    <div>
-      <h1>Bye bye world</h1>
+    <div className="flex flex-col flex-grow px-6 pt-1">
+      <h5 className="my-3 font-medium">
+        I am currently pursuing B.Tech Degree(Final Year) in Computer Science
+        Engineering from Academy of Technology. I have 3+ years of experience in
+        Web Development and I have a Youtube Channel where I teach Full Stack
+        Web Development
+      </h5>
+      <div
+        className="flex-grow p-4 mt-4 bg-gray-400"
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+      >
+        <h6 className="my-3 text-xl font-bold tracking-wide">What I Offer</h6>
+        <div className="grid gap-6 lg:grid-cols-2">
+          {/* {props.services.map((service) => (
+            <div className="bg-gray-200 rounded-lg lg:col-span-1 ">
+              <ServiceCard service={service} />
+            </div>
+          ))} */}
+          {services.map((service) => (
+            <div className="bg-gray-200 rounded-lg lg:col-span-1 ">
+              <ServiceCard service={service} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default index;
 
- //If ourt data is static we dont need to use getServerSideProps() function
+//If ourt data is static we dont need to use getServerSideProps() function
 // export const getServerSideProps = async (contex: GetServerSideProps) => {
 //   //calculation
 
@@ -33,6 +58,7 @@ export default index;
 // };
 
 
+
 // This functon call once time at the project build beacuse this data static and never change so we dont need call every About page opening
 export const getStaticProps = async (contex: GetServerSideProps) => {
   //calculation
@@ -40,7 +66,7 @@ export const getStaticProps = async (contex: GetServerSideProps) => {
   const res = await fetch("http://localhost:3000/api/services");
   const data = await res.json();
 
-  console.log('getStaticProps Metod')
+  console.log("getStaticProps Metod");
   // console.log("----------------------------Server----------------------------");
   // console.log("SERVER", data);
 
@@ -50,4 +76,3 @@ export const getStaticProps = async (contex: GetServerSideProps) => {
     },
   };
 };
-
