e554cd4551b13685674d97296b503455d974ba91
Zeki Kaya <zeki.kaya@amerikapostam.com>
2021-11-24 22:18:09 +0300

    ProjectNavbar component created
    using  project by navbar

---
+++ components/ProjectsNavbar.tsx
@@ -0,0 +1,39 @@
+import React, { FunctionComponent } from "react";
+import { Category } from "../type";
+
+const NavItem: FunctionComponent<{
+  value: Category | "all";
+  handlerFilterCategory: Function;
+  active: string;
+}> = ({ value, handlerFilterCategory,active }) => {
+
+    let clasName = "capitalize cursor-pointer hover:text-green"
+    if(active== value)
+        clasName += " text-green" 
+    
+  return (
+    <li
+      className={clasName}
+      onClick={() => handlerFilterCategory(value)}
+    >
+      {value}
+    </li>
+  );
+};
+
+const ProjectsNavbar: FunctionComponent<{
+  handlerFilterCategory: Function;
+  active: string;
+}> = (props) => {
+  return (
+    <div className="flex px-3 py-2 space-x-3 overflow-x-auto list-none">
+      <NavItem value="all" {...props} />
+      <NavItem value="react" {...props} />
+      <NavItem value="mongo" {...props} />
+      <NavItem value="django" {...props} />
+      <NavItem value="node" {...props} />
+    </div>
+  );
+};
+
+export default ProjectsNavbar;
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectCard from "../components/ProjectCard";
-import { projects } from "../data";
+import ProjectsNavbar from "../components/ProjectsNavbar";
+import { projects as projectsData } from "../data";
+import { Category } from "../type";
 
 const Projects = () => {
+  const [projects, setProjects] = useState(projectsData);
+  const [activeItem, setActiveItem] = useState("all");
+
+  const handlerFilterCategory = (category: Category | "all") => {
+    if (category == "all") {
+      setProjects(projectsData);
+      setActiveItem("all");
+      return;
+    }
+
+    const newArray = projectsData.filter((project) =>
+      project.category.includes(category)
+    );
+
+    setProjects(newArray);
+    setActiveItem(category);
+  };
+
   return (
-    <div className='px-5 py-2 overflow-y-scroll' style={{height:'65vh'}}>
-      <nav>Navbar</nav>
+    <div className="px-5 py-2 overflow-y-scroll" style={{ height: "65vh" }}>
+      <ProjectsNavbar
+        handlerFilterCategory={handlerFilterCategory}
+        active={activeItem}
+      />
 
       <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
